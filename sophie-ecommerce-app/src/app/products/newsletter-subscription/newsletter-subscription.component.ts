139c18dd0945a3e8c0dae8b09a8d1bc5b98c6f91
Ebere <ebereuzodufa@gmail.com>
2020-01-22 04:15:11 +0100

    Used newsletter component

---
+++ sophie-ecommerce-app/src/app/products/newsletter-subscription/newsletter-subscription.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products',
+  templateUrl: './newsletter-subscription.component.html',
+  styleUrls: ['./newsletter-subscription.component.scss']
+})
+export class NewsletterSubscriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
