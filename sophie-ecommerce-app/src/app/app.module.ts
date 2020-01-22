7e1a65aabe1a171360e8a39da26bf520ec8b568b
Ebere <ebereuzodufa@gmail.com>
2020-01-22 01:31:53 +0100

    Wires components & route up in app.module

--- sophie-ecommerce-app/src/app/app.module.ts
+++ sophie-ecommerce-app/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { SiteFooterComponent } from './siteFooter/siteFooter.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteFooterComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
