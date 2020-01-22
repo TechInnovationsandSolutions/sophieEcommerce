12b1109382c6facd1723fa9942c180842d161d47
Ebere <ebereuzodufa@gmail.com>
2020-01-22 03:50:38 +0100

    Registers site-footer component on app.module

--- sophie-ecommerce-app/src/app/app.module.ts
+++ sophie-ecommerce-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import { SiteFooterComponent } from './siteFooter/siteFooter.component';
+import { SiteFooterComponent } from './siteFooter/site-footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
