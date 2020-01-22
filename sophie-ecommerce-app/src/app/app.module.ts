d71e358aa21500e338771ee573325d4c53408d79
Ebere <ebereuzodufa@gmail.com>
2020-01-22 04:08:28 +0100

    Creates home product component & registers in app

--- sophie-ecommerce-app/src/app/app.module.ts
+++ sophie-ecommerce-app/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 import { SiteFooterComponent } from './siteFooter/site-footer.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeProductsComponent } from './products/home-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SiteFooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ sophie-ecommerce-app/src/app/products/home-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'home-products',
+  templateUrl: './home-products.component.html',
+  styleUrls: ['./home-products.component.scss']
+})
+export class HomeProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
