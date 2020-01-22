5504d6980431d8765479d1a1d3a83f0d4a06e8ca
Ebere <ebereuzodufa@gmail.com>
2020-01-22 04:14:53 +0100

    Used category-thumbnail component

---
+++ sophie-ecommerce-app/src/app/products/category-thumbnail/category-thumbnail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products',
+  templateUrl: './category-thumbnail.component.html',
+  styleUrls: ['./category-thumbnail.component.scss']
+})
+export class CategoryThumbnailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
