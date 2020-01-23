ee9fc1a09991d5c59a37990887454b7df83540d1
Ebere <ebereuzodufa@gmail.com>
2020-01-23 12:36:45 +0100

    Set up some common routes

--- sophie-ecommerce-app/src/app/routes.ts
+++ sophie-ecommerce-app/src/app/routes.ts
@@ -1,5 +1,21 @@
 import { Routes } from "@angular/router";
+import { HomeProductsComponent, AboutPageComponent, ContactPageComponent,PrivacyPageComponent } from "./index";
 
 export const appRoutes:Routes =[
-
+    {
+        path: '',
+        component: HomeProductsComponent
+    },
+    {
+        path:'about',
+        component: AboutPageComponent
+    },
+    {
+        path: 'contact',
+        component: ContactPageComponent
+    },
+    {
+        path: 'privacy-policy',
+        component: PrivacyPageComponent
+    }
 ]
