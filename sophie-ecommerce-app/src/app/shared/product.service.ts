a3506da6c4a369e13c7cd30e38ee7585c4e9ea66
Ebere <ebereuzodufa@gmail.com>
2020-01-28 23:38:46 +0100

    gets json data in product service

--- sophie-ecommerce-app/src/app/shared/product.service.ts
+++ sophie-ecommerce-app/src/app/shared/product.service.ts
@@ -1,6 +1,32 @@
 import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+import { IProduct, IUSer, ICategory } from './model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ProductService{
+    private _users = '../assets/data/users.json';
+    private _products = '../assets/data/products.json';
+    private _category = '../assets/data/category.json';
+    private _social_media = '../assets/data/social_media.json';
+    private _testimonial = '../assets/data/testimonial.json';
+    private _contact_details = '../assets/data/contact_details.json';
 
+    constructor(private _http:HttpClient){}
+
+    getProducts(): Observable<IProduct[]>{
+        return this._http.get(this._products).pipe(map(resp=><IProduct[]>resp));
+    }
+
+    getPopularProducts(){
+        return new Promise(resolve=>{
+            var popularProducts:IProduct[];
+            this.getProducts().subscribe(response =>{
+                popularProducts = response.filter(p=>!!p.isPopular);
+                console.log('oso', popularProducts);
+                resolve(popularProducts)
+            });
+        });
+    }
 }
