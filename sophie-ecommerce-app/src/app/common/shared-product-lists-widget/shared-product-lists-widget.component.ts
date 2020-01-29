e019b89f1e1899d99ac795819b6445149a354d51
Ebere <ebereuzodufa@gmail.com>
2020-01-29 02:42:47 +0100

    Set shared widget to dsplay using imputs

--- sophie-ecommerce-app/src/app/common/shared-product-lists-widget/shared-product-lists-widget.component.ts
+++ sophie-ecommerce-app/src/app/common/shared-product-lists-widget/shared-product-lists-widget.component.ts
@@ -1,16 +1,16 @@
 //To create a product-list with title like popular products or related products.
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IProduct } from 'src/app/shared';
 
 @Component({
   selector: 'shared-product-lists-widget',
   templateUrl: './shared-product-lists-widget.component.html',
   styleUrls: ['./shared-product-lists-widget.component.scss']
 })
-export class SharedProductListsWidgetComponent implements OnInit {
+export class SharedProductListsWidgetComponent{
 
+  @Input() theProducts:IProduct[];
+  @Input() widgetTitle:string;
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
