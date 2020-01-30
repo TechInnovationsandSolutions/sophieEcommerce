cef44e147d1c014440733891c34afbc0cb4aca55
Ebere <ebereuzodufa@gmail.com>
2020-01-30 03:42:28 +0100

    creates site footer component

--- sophie-ecommerce-app/src/app/siteFooter/site-footer.component.ts
+++ sophie-ecommerce-app/src/app/siteFooter/site-footer.component.ts
@@ -1,10 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from '../shared';
 
 @Component({
     selector: 'site-footer',
     templateUrl: './site-footer.component.html'
 })
 
-export class SiteFooterComponent{
+export class SiteFooterComponent implements OnInit{
+    socialMedia: any[];
+    contactDetails: any[];
 
+    constructor(private productService: ProductService){}
+
+    ngOnInit() {
+        this.productService.getContactDetails().subscribe(resp=>{
+            this.contactDetails = resp;
+        })
+
+        this.productService.getSocialMedia().subscribe(resp=>{
+            this.socialMedia = resp;
+        })
+    }
 }
