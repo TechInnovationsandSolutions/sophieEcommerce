dd40b0e3b6af09cb2c61fc5f0b7dbdb27e4bc925
Ebere <ebereuzodufa@gmail.com>
2020-02-08 22:13:33 +0100

    Creates module declarations & import for users

--- sophie-ecommerce-app/src/app/user/user.module.ts
+++ sophie-ecommerce-app/src/app/user/user.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { userRoutes } from './user.routes';
 
 
 
 @NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(userRoutes)
+  ],
   declarations: [
     LoginComponent, 
     RegisterComponent, 
     ProfileComponent, 
     ForgotPasswordComponent
-  ],
-  imports: [
-    CommonModule
   ]
 })
 export class UserModule { }
