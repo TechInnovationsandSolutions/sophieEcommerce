14e1eef13ba59b96c7887492dc8e964140169a15
Ebere <ebereuzodufa@gmail.com>
2020-02-09 01:27:43 +0100

    use boolean to toggle login

--- sophie-ecommerce-app/src/app/user/login-register/login-register.component.ts
+++ sophie-ecommerce-app/src/app/user/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-register',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-register.component.scss']
 })
 export class LoginRegisterComponent implements OnInit {
+  isLogin: boolean = false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    var routes = this.router.url.split('/');
+    var route = routes[2].toString().toLowerCase();
+
+    (route === 'login') ? (this.isLogin = true) : (this.isLogin = false);
+    console.log(this.router.url, routes, route)
   }
 
 }
