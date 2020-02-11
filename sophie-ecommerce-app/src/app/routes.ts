743b4cb2b14845ee5d9cde81b68a4a2b309dce12
Ebere <ebereuzodufa@gmail.com>
2020-02-11 02:04:34 +0100

    Set path for individual products

--- sophie-ecommerce-app/src/app/routes.ts
+++ sophie-ecommerce-app/src/app/routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from "@angular/router";
 import { HomeProductsComponent, AboutPageComponent, ContactPageComponent,PrivacyPageComponent, ReturnPolicyPageComponent } from "./index";
+import { ProductDetailsComponent } from './products';
 
 export const appRoutes:Routes =[
     {
         path: '',
         component: HomeProductsComponent
+    },
+    {
+        path: 'product/:id',
+        component: ProductDetailsComponent
     },
     {
         path:'about',
@@ -21,6 +26,16 @@ export const appRoutes:Routes =[
     {
         path: 'return-policy',
         component: ReturnPolicyPageComponent
+    },
+    {
+        path:'product', 
+        redirectTo: '', 
+        pathMatch: 'full'
+    },
+    {
+        path:'products', 
+        redirectTo: '', 
+        pathMatch: 'full'
     },
     {
         path: 'user',
