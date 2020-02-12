7366e68c2073a0b17fc14873ece376a564948894
Ebere <ebereuzodufa@gmail.com>
2020-02-12 01:08:10 +0100

    creates and styles single product page

--- sophie-ecommerce-app/src/app/products/product-details/product-details.component.ts
+++ sophie-ecommerce-app/src/app/products/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct, ProductService } from 'src/app/shared';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'product-details',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product:IProduct;
+  isReview: boolean;
 
-  constructor() { }
+  constructor(private productService: ProductService, private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.forEach((params:Params)=>{
+      this.productService.getProduct(+params['id']).then(res=>{
+        console.log('the product',res)
+        this.product = <IProduct>res[0];
+      });
+    })
   }
 
+  showReview(){
+    this.isReview = true;
+  }
+
+  showDescription(){
+    this.isReview = false;
+  }
 }
