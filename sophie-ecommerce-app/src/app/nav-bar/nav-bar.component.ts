3175e469f306dc5da2a1aeb087099e8ce42ca340
Ebere <ebereuzodufa@gmail.com>
2020-02-14 16:13:57 +0100

    enables collapse of nav bar on mobile

--- sophie-ecommerce-app/src/app/nav-bar/nav-bar.component.ts
+++ sophie-ecommerce-app/src/app/nav-bar/nav-bar.component.ts
@@ -1,11 +1,32 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent{
+export class NavBarComponent implements OnInit{
+
+  ngOnInit(){
+    this.clickLink();
+  }
 
   constructor() { }
+
+  collapseIfMobile(){
+    var toggleBtn = <HTMLElement>document.querySelector('button.navbar-toggler');
+    var isToggleBtnVisible = (window.getComputedStyle(toggleBtn)['display'] != 'none') ? true : false;
+
+    if (isToggleBtnVisible) {
+      toggleBtn.click();
+    }
+  }
+
+  clickLink(){
+    window.onload = e=>{
+      document.querySelectorAll('a.nav-link').forEach(link => {
+        link.addEventListener('click', this.collapseIfMobile);
+      });
+    }
+  }
 }
