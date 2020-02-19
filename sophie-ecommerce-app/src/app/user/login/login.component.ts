dd41d5de16c3a422e64a755fd57dbf4021667121
Ebere <ebereuzodufa@gmail.com>
2020-02-19 02:14:03 +0100

    Implements dummy login

--- sophie-ecommerce-app/src/app/user/login/login.component.ts
+++ sophie-ecommerce-app/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
+  wrongAuth: boolean = false;
 
-  constructor() { }
+  constructor(private auth:AuthService, private fb:FormBuilder, private routr: Router) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    })
   }
 
+  resetForm(){
+    this.wrongAuth = false;
+  }
+
+  submitForm(form){
+    console.log('login f', form);
+    const resp = this.auth.loginUser(form.username, form.password);
+    console.log('resp', resp);
+
+    if(resp){
+      this.routr.navigate(['/products']);
+    } else{
+      this.wrongAuth = true;
+    }
+  }
 }
