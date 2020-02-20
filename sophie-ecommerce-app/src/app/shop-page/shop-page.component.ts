e3f2c55faa5a800852fa1ecdecf8deaece4c8415
Ebere <ebereuzodufa@gmail.com>
2020-02-20 17:26:39 +0100

    creates component to show all shop items

--- sophie-ecommerce-app/src/app/shop-page/shop-page.component.ts
+++ sophie-ecommerce-app/src/app/shop-page/shop-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService, ICategory, IProduct } from '../shared';
 
 @Component({
   selector: 'shop-page',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopPageComponent implements OnInit {
 
-  constructor() { }
+  categories:ICategory[];
+  isMobile:boolean;
+
+  products:IProduct[] = [];
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getProducts().subscribe(res=>{
+      console.log(res);
+      this.products = <IProduct[]>res;
+      console.log('products', this.products);
+    })
+
+    this.isMobile = this.getIsMobile();
+    window.onresize = () => {
+      this.isMobile = this.getIsMobile();
+    };
+    this.productService.getCategories().subscribe(resp=>{
+      this.categories = resp;
+      console.log('categ', this.categories);
+    })
   }
 
+  getIsMobile(): boolean {
+    const w = document.documentElement.clientWidth;
+    const breakpoint = 768;
+    // console.log(w);
+    if (w < breakpoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
