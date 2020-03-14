417c817f2af23fcc2c05ae83c0ba7dbb5a153f17
Ebere <ebereuzodufa@gmail.com>
2020-03-14 17:17:19 +0100

    creates fn for route guard based on authentication

--- sophie-ecommerce-app/src/app/shared/auth-route-guard.guard.ts
+++ sophie-ecommerce-app/src/app/shared/auth-route-guard.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ProductService } from './product.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthRouteGuardGuard implements CanActivate {
+  constructor(private serv: ProductService, private router: Router){}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    const redirectUrl = next['_routerState']['url'];
+
+    if (this.serv.isLogged()) {
+      return true;
+    }
+
+    this.router.navigateByUrl(
+      this.router.createUrlTree(
+        ['/login'], {
+          queryParams: {
+            redirectUrl
+          }
+        }
+      )
+    );
+
+    return false;
   }
   
 }
