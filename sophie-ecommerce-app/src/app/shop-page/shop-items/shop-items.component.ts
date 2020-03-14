9224beb8298d9f1974a49da6fe39f7793241f5db
Ebere <ebereuzodufa@gmail.com>
2020-03-14 17:15:42 +0100

    makes good display of products on mobile, & few no

--- sophie-ecommerce-app/src/app/shop-page/shop-items/shop-items.component.ts
+++ sophie-ecommerce-app/src/app/shop-page/shop-items/shop-items.component.ts
@@ -9,96 +9,51 @@ import { IProduct, ProductService } from 'src/app';
 export class ShopItemsComponent implements OnInit {
 
   @Input() shopProducts:IProduct[];
-  isPaginated:boolean = false;
   currentPageProducts:IProduct[] = []
   
   constructor(private productService:ProductService) { }
 
   ngOnInit() {
+    console.log('this.shopProducts', this.shopProducts)
     setTimeout(() => {
       this.currentPageProducts = this.shopProducts;
     }, 500);
   }
 
   change(){
-    this.currentPageProducts = [
-      {
-        "id": 1,
-        "name": "Toning Oil 1",
-        "category": {
-            "id": 1,
-            "name": "skin type",
-            "slug": "skin-type"
-        },
-        "description": [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non in morbi luctus sed amet amet risus ligula. Neque diam tincidunt est praesent sed tortor. In facilisi urna amet nec habitant eget. Faucibus hendrerit quam viverra sit malesuada nulla in. Donec lacus molestie duis et habitant sed.",
-            "Duis orci odio sagittis mauris dignissim. Enim, nisl velit feugiat malesuada velit facilisi. Nunc odio nisl, feugiat nec arcu adipiscing. Id vestibulum m",
-            "Faucibus donec nibh non tempor erat mauris tempor. Justo, viverra consequat leo."
-        ],
-        "excerpts": "Toning Oil dolor does amet, consectetur adipiscing elit. Convallis at egestas magna purus sit urna, eu.Convallis  mi mattis mattis vel tellus ",
-        'price':5000,
-        'promoPrice':5000,
-        "imageURL": "/assets/images/product-1.png",
-        "createdOn":"1579737786364",
-        "lastUpdate":"1579737786364",
-        "availibility": "true",
-        "review": [
-            {
-                "name":"Cross",
-                "reviewSummary": "Mad Ohh!",
-                "rating": 2.5,
-                "reviewMessage": "This is just for testing purpose only. You dig?"
-            }
-        ],
-        "tag": ["Lotion", "Oil", "toning", "Acne", "cream", "dark"],
-        "isPopular": true,
-        "ratingAverage": 1.24
-    },
-    {
-        "id": 2,
-        "name": "Toning Oil 2",
-        "category": {
-            "id": 2,
-            "name": "skin concern",
-            "slug": "skin-concern"
-        },
-        "description": [
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non in morbi luctus sed amet amet risus ligula. Neque diam tincidunt est praesent sed tortor. In facilisi urna amet nec habitant eget. Faucibus hendrerit quam viverra sit malesuada nulla in. Donec lacus molestie duis et habitant sed.",
-            "Duis orci odio sagittis mauris dignissim. Enim, nisl velit feugiat malesuada velit facilisi. Nunc odio nisl, feugiat nec arcu adipiscing. Id vestibulum m",
-            "Faucibus donec nibh non tempor erat mauris tempor. Justo, viverra consequat leo."
-        ],
-        "excerpts": "Toning Oil dolor does amet, consectetur adipiscing elit. Convallis at egestas magna purus sit urna, eu.Convallis  mi mattis mattis vel tellus ",
-        'price':5000,
-        "promoPrice":4500,
-        "imageURL": "/assets/images/product-2.png",
-        "createdOn":"1579737786364",
-        "lastUpdate":"1579737786364",
-        "availibility": "true",
-        "review": [
-            {
-                "name":"Cross",
-                "reviewSummary": "Mad Ohh!",
-                "rating": 2.5,
-                "reviewMessage": "This is just for testing purpose only. You dig?"
-            },
-            {
-                "name":"Mako",
-                "reviewSummary": "Mad Ohh!",
-                "rating": 2.5,
-                "reviewMessage": "This is just for testing purpose only. You dig?"
-            },
-            {
-                "name":"Sydney",
-                "reviewSummary": "Mad Ohh!",
-                "rating": 2.5,
-                "reviewMessage": "This is just for testing purpose only. You dig?"
-            }
-        ],
-        "tag": ["Lotion", "cream", "dark"],
-        "isPopular": true,
-        "ratingAverage": 3.24
-      }
-    ]
+    // this.currentPageProducts = [
+    //   {
+    //     id: 1,
+    //     name: "Toning Oil 1",
+    //     category: {
+    //         id: 1,
+    //         name: "skin type",
+    //         slug: "skin-type"
+    //     },
+    //     description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non in morbi luctus sed amet amet risus ligula. Neque diam tincidunt est praesent sed tortor. In facilisi urna amet nec habitant eget. Faucibus hendrerit quam viverra sit malesuada nulla in. Donec lacus molestie duis et habitant sed. Duis orci odio sagittis mauris dignissim. Enim, nisl velit feugiat malesuada velit facilisi. Nunc odio nisl, feugiat nec arcu adipiscing. Id vestibulum m Faucibus donec nibh non tempor erat mauris tempor. Justo, viverra consequat leo.",
+    //     excerpt: "Toning Oil dolor does amet, consectetur adipiscing elit. Convallis at egestas magna purus sit urna, eu.Convallis  mi mattis mattis vel tellus" ,
+    //     cost:5000,
+    //     reduced_cost:5000,
+    //     images: [
+    //       {
+    //         url : "/assets/images/product-1.png"
+    //       }
+    //     ],
+    //     createdOn:"1579737786364",
+    //     lastUpdate:"1579737786364",
+    //     availibility: "true",
+    //     ratings: [
+    //         {
+    //             name:"Cross",
+    //             rating: 2.5,
+    //             comment: "This is just for testing purpose only. You dig?"
+    //         }
+    //     ],
+    //     tags: ["Lotion", "Oil", "toning", "Acne", "cream", "dark"],
+    //     avg_rating: 1.24,
+    //     quantity: 5
+    // }
+    // ]
   }
-
 }
