6cc0122f3c964420b46656d1a8b683df6399a07e
Ebere <ebereuzodufa@gmail.com>
2020-05-02 00:37:17 +0100

    Implements contact message

--- sophie-ecommerce-app/src/app/contact-page/contact-page.component.ts
+++ sophie-ecommerce-app/src/app/contact-page/contact-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../shared';
+import { ProductService, IContact } from '../shared';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -9,20 +10,30 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
-  constructor(private productService: ProductService, private fb: FormBuilder) { }
-
   contactDetails: any[];
+  loading = false;
+
   contactForm = this.fb.group({
     name: ['', [Validators.required]],
-    message: ['', [Validators.required]],
+    subject: [''],
+    body: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]]
   });
+
+  constructor(
+    private productService: ProductService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
     this.productService.makeSEO('Contacts');
     this.productService.getContactDetails().subscribe(resp => {
         this.contactDetails = resp;
     });
+  }
+
+  clearForm() {
+    this.contactForm.reset({});
   }
 
   canDeactivate() {
@@ -32,5 +43,19 @@ export class ContactPageComponent implements OnInit {
     }
 
     return true;
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const msg: IContact = this.contactForm.value;
+      this.loading = true;
+      this.productService.contactUs(msg).then(r => {
+        this.loading = false;
+        if (r.status === 'success') {
+          Swal.fire('Message Sent', 'Hi ' + msg.name + '. Your message has been received. Thank you.', 'success');
+          this.clearForm();
+        }
+      });
+    }
   }
 }
