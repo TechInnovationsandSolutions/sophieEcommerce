6bdd4fd15ee590112c71727debbb140f2c3f81a3
Ebere <ebereuzodufa@gmail.com>
2020-05-02 01:02:37 +0100

    Implements newsletter

--- sophie-ecommerce-app/src/app/products/newsletter-subscription/newsletter-subscription.component.ts
+++ sophie-ecommerce-app/src/app/products/newsletter-subscription/newsletter-subscription.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app';
+import { Validators, FormBuilder } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
+  // tslint:disable-next-line: component-selector
   selector: 'newsletter-subscription',
   templateUrl: './newsletter-subscription.component.html',
   styleUrls: ['./newsletter-subscription.component.scss']
 })
 export class NewsletterSubscriptionComponent implements OnInit {
+  loading = false;
+  newsLetterForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]]
+  });
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
   }
 
+  subNewsLetter() {
+    if (this.newsLetterForm.valid) {
+      const useremail = this.newsLetterForm.value.email;
+      this.loading = true;
+      this.productService.newsletterSub(useremail).then(r => {
+        this.loading = false;
+        if (r.status === 'success') {
+          Swal.fire('Thank you for Subscribing', '', 'success');
+          this.newsLetterForm.reset({});
+        }
+      });
+    }
+  }
 }
