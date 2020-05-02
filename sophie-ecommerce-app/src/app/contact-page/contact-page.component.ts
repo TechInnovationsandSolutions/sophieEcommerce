8f880ea42bbc7217f2fa303178b6fdf96df753f0
Ebere <ebereuzodufa@gmail.com>
2020-05-02 12:35:41 +0100

    Adds default message if no subject

--- sophie-ecommerce-app/src/app/contact-page/contact-page.component.ts
+++ sophie-ecommerce-app/src/app/contact-page/contact-page.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService, IContact } from '../shared';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import Swal from 'sweetalert2';
+import { FirstCharCapitalizePipe } from '../common/first-char-capitalize.pipe';
 
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'contact-page',
   templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.scss']
+  styleUrls: ['./contact-page.component.scss'],
+  providers: [FirstCharCapitalizePipe]
 })
 export class ContactPageComponent implements OnInit {
   contactDetails: any[];
@@ -22,7 +24,8 @@ export class ContactPageComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private firstChar: FirstCharCapitalizePipe
   ) { }
 
   ngOnInit() {
@@ -48,6 +51,8 @@ export class ContactPageComponent implements OnInit {
   onSubmit() {
     if (this.contactForm.valid) {
       const msg: IContact = this.contactForm.value;
+      const defaultSubject = 'Contact message from Sophie Bath and Body';
+      msg.subject = msg.subject ? (this.firstChar.transform(msg.subject) + ' - ' + defaultSubject) : defaultSubject;
       this.loading = true;
       this.productService.contactUs(msg).then(r => {
         this.loading = false;
