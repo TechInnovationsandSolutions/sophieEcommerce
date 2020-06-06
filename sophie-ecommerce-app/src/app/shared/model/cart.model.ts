a34b7d7f0f03751a31453d42b861c0aeae34c16f
unknown <ebereuzodufa@gmail.com>
2020-06-06 21:17:23 +0100

    Add maxQty field

--- sophie-ecommerce-app/src/app/shared/model/cart.model.ts
+++ sophie-ecommerce-app/src/app/shared/model/cart.model.ts
@@ -1,9 +1,10 @@
 export interface ICart {
-    id?: number;
-    product: string;
-    product_id?: number;
-    amount: number;
-    amount_main?: number;
-    quantity: number;
-    imgUrl: string;
+  id?: number;
+  product: string;
+  product_id?: number;
+  amount: number;
+  amount_main?: number;
+  quantity: number;
+  imgUrl: string;
+  maxQty?: number;
 }
