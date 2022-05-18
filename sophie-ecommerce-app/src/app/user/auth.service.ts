209aea90c7d6fb9322677d2016bd0652dc7608f3
Ebere <ebereuzodufa@gmail.com>
2022-05-18 09:10:39 +0100

    Adds base endpoint

--- sophie-ecommerce-app/src/app/user/auth.service.ts
+++ sophie-ecommerce-app/src/app/user/auth.service.ts
@@ -1,31 +1,32 @@
-import { Injectable } from '@angular/core';
-import { IUserReg, IUSer, ProductService } from '../shared';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { IUserReg, IUSer, ProductService } from "../shared";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
 
-const USER = 'x-user';
-const TOKEN = 'x-token';
+const USER = "x-user";
+const TOKEN = "x-token";
 
 @Injectable()
 export class AuthService {
-  baseUrl = 'https://tis-bandb.herokuapp.com/api/v1/'; // Base URL
+  baseUrl = "http://ec2-52-87-173-131.compute-1.amazonaws.com/"; // Base URL
   // tslint:disable-next-line: variable-name
-  _url = this.baseUrl + 'auth/';
+  _url = this.baseUrl + "auth/";
 
   currentUser: IUSer;
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
     return localStorage.getItem(TOKEN);
   }
 
   setUser(user: string): void {
-      localStorage.setItem(USER, user);
+    localStorage.setItem(USER, user);
   }
 
   getUser() {
@@ -42,19 +43,21 @@ export class AuthService {
   }
 
   registerUser(user: IUserReg) {
-    return this.http.post<any>(this._url + 'register', {
-      email: user.email,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      phone: user.phone,
-      password: user.password,
-      password_confirmation: user.password,
-    }).toPromise();
+    return this.http
+      .post<any>(this._url + "register", {
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone: user.phone,
+        password: user.password,
+        password_confirmation: user.password,
+      })
+      .toPromise();
   }
 
   loginUser(userEmail: string, password: string): Observable<any> {
     if (userEmail && password) {
-      return this.http.post<any>(this._url + 'login', {
+      return this.http.post<any>(this._url + "login", {
         email: userEmail,
         password,
       });
@@ -63,33 +66,45 @@ export class AuthService {
 
   updateUser(user: IUSer) {
     const token = this.getToken();
-    return this.http.post<any>(this._url + 'update', {
-      first_name: user.first_name,
-      last_name: user.last_name,
-      phone: user.phone,
-      address: 't'
-    }, {
-      headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
-    }).toPromise();
+    return this.http
+      .post<any>(
+        this._url + "update",
+        {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          phone: user.phone,
+          address: "t",
+        },
+        {
+          headers: new HttpHeaders().set("Authorization", `Bearer ${token}`),
+        }
+      )
+      .toPromise();
   }
 
   updatePassword(newPassword: string) {
     const token = this.getToken();
-    return this.http.post<any>(this._url + 'update-password', {
-        newPassword,
-    }, {
-        headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
-    }).toPromise();
+    return this.http
+      .post<any>(
+        this._url + "update-password",
+        {
+          newPassword,
+        },
+        {
+          headers: new HttpHeaders().set("Authorization", `Bearer ${token}`),
+        }
+      )
+      .toPromise();
   }
 
   isAuthenticated(): boolean {
     let isLegit = false;
 
     if (this.currentUser) {
-        isLegit =  true;
+      isLegit = true;
     } else if (this.isGotUser()) {
-        this.currentUser = this.getUser();
-        isLegit = true;
+      this.currentUser = this.getUser();
+      isLegit = true;
     }
     return isLegit;
     // return !!this.currentUser;
@@ -98,17 +113,23 @@ export class AuthService {
   logOut() {
     this.currentUser = null;
     this.removeUser();
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
   }
 
   getAuthenticatedUser(userEmail: string, password: string) {
     const token = this.getToken();
-    return this.http.post<any>(this._url + 'user', {
-      userEmail,
-      password
-    }, {
-        headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
-    }).toPromise();
+    return this.http
+      .post<any>(
+        this._url + "user",
+        {
+          userEmail,
+          password,
+        },
+        {
+          headers: new HttpHeaders().set("Authorization", `Bearer ${token}`),
+        }
+      )
+      .toPromise();
   }
 }
 
